cd04dae12114e831f583ecb6e77d4070a804c0c2
Yulan van Oppen <yulanvanoppen@gmail.com>
2024-10-10 11:08:43 +0200

    Update

--- ATP_AMP/Qmat.m
+++ ATP_AMP/Qmat.m
@@ -1,3 +1,6 @@
+% UNCOMMENT Q-MATRIX OF INTEREST
+
+% % Candidate configuration
 % function Q = Qmat(k, T, M)
 %     %  0 M0   1-M    2-Ma    3-M1   4-M1a   5-M2a  6-M2aa  7-Maa  8-M1aa  9-M3aa
 %     Q = [0      k(17)  k(19)   k(21)  k(23)   0      0       0      0       0    ;        % 0: M0
@@ -13,7 +16,7 @@
 %     Q = Q - diag(sum(Q, 2));
 % end
 
-
+% % Alternative: Equal rates Ma <-> M2a and M2aa <-> Maa
 % function Q = Qmat(k, T, M)
 %     %  0 M0   1-M    2-Ma    3-M1   4-M1a   5-M2a  6-M2aa  7-Maa  8-M1aa  9-M3aa
 %     Q = [0      k(17)  k(19)   k(21)  k(23)   0      0       0      0       0    ;        % 0: M0
@@ -29,6 +32,7 @@
 %     Q = Q - diag(sum(Q, 2));
 % end
 
+% Alternative: Additional transition Ma -> Maa
 function Q = Qmat(k, T, M)
     %  0 M0   1-M    2-Ma    3-M1   4-M1a   5-M2a  6-M2aa  7-Maa  8-M1aa  9-M3aa
     Q = [0      k(17)  k(19)   k(21)  k(23)   0      0       0      0       0    ;        % 0: M0
